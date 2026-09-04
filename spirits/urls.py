5a792e6fc22cf86b0e673232d4f82cb275b0b8c1
Ethan Chandler <marioesc425@gmail.com>
2026-09-04 17:28:49 -0400

    Add spirit detail view with URL routing and template

--- spirits/urls.py
+++ spirits/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.spirit_list, name='spirit_list'),
+    path('<int:pk>/', views.spirit_detail, name='spirit_detail')
 ]
--- spirits/views.py
+++ spirits/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Spirit
-# Create your views here.
+
 def spirit_list(request):
     spirits = Spirit.objects.all()
     return render(request, 'spirits/spirit_list.html', {'spirits': spirits})
+
+def spirit_detail(request, pk):
+    spirit = get_object_or_404(Spirit, pk=pk)
+    return render(request, 'spirits/spirit_detail.html', {'spirit': spirit})
